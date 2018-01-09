d1c051e6d4bc6b78d24b419f954f4a1a5a937415
Long Nguyen <all4onegiallorossi@gmail.com>
2018-01-10 08:26:05 +0900

    new up

--- Prototype2/Controller/LoginVC.swift
+++ Prototype2/Controller/LoginVC.swift
@@ -8,20 +8,29 @@
 
 import UIKit
 
-class LoginVC: UIViewController {
+class LoginVC: UIViewController,UITextFieldDelegate {
 
     @IBOutlet weak var tfPassword: UITextField!
     @IBOutlet weak var tfUsername: UITextField!
+    @IBOutlet weak var tfShopPassword: UITextField!
+    @IBOutlet weak var tfShopId: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tfUsername.delegate = self
+        tfPassword.delegate = self
+        tfShopPassword.delegate = self
+        tfShopId.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btnLoginQRCodePressed(_ sender: Any) {
+        
     }
 
     @IBAction func btnLoginPressed(_ sender: Any) {
@@ -34,5 +43,55 @@ class LoginVC: UIViewController {
                 }
             })
         }
+    }
+    
+    //get the cloud database
+    func loadCloudDatabase(){
+//        let container = CKContainer.default()
+//        let privateDatabase = container.publicCloudDatabase
+//        let predicate = NSPredicate(value: true)
+//
+//        let query = CKQuery(recordType: "CustomerData", predicate: predicate)
+//
+//        privateDatabase.perform(query, inZoneWith: nil) { (results, error) -> Void in
+//            if error != nil {
+//                print(error!)
+//                SVProgressHUD.showError(withStatus: error?.localizedDescription)
+//            }
+//            else {
+//                print(results!)
+//
+//                for result in results! {
+//                    GlobalVariables.sharedManager.cusRecord.append(result)
+//                }
+//
+//                OperationQueue.main.addOperation({ () -> Void in
+//                    SVProgressHUD.dismiss()
+//                    self.goToMainPage()
+//                })
+//            }
+//        }
+    }
+    
+    func goToMainPage(){
+//        let storyboard = UIStoryboard(name: "MainScene", bundle: nil)
+//        let controller = storyboard.instantiateViewController(withIdentifier: "MainPageViewController") as! MainPageVC
+//
+//        self.present(controller, animated: true, completion: nil)
+    }
+    
+    //MARK: - Textfield Delegate
+    /***************************************************************/
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //MARK: - UIResponder Delegate
+    /***************************************************************/
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 }
