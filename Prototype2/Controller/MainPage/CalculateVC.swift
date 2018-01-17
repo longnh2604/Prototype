84aab27528fb00987f9ba1ab4eca62185fac1706
Long <longnh264@gmail.com>
2018-01-17 19:31:02 +0900

    update them

---
+++ Prototype2/Controller/MainPage/CalculateVC.swift
@@ -0,0 +1,41 @@
+//
+//  CalculateVC.swift
+//  Prototype2
+//
+//  Created by Long on 2018/01/17.
+//  Copyright © 2018 Long. All rights reserved.
+//
+
+import UIKit
+
+class CalculateVC: UIViewController  {
+
+    @IBOutlet weak var imvPolygon: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        let shape = CAShapeLayer()
+//
+//        shape.opacity = 0.5
+//        shape.lineWidth = 2
+//        shape.lineJoin = kCALineJoinMiter
+//        shape.strokeColor = UIColor(hue: 0.786, saturation: 0.79, brightness: 0.53, alpha: 1.0).cgColor
+//        shape.fillColor = UIColor(hue: 0.786, saturation: 0.15, brightness: 0.89, alpha: 1.0).cgColor
+//        view.layer.addSublayer(shape)
+//
+//        let path = UIBezierPath()
+//        path.move(to: CGPoint(x: 120, y: 20))
+//        path.addLine(to: CGPoint(x: 230, y: 90))
+//        path.addLine(to: CGPoint(x: 240, y: 250))
+//        path.addLine(to: CGPoint(x: 100, y: 150))
+//        path.close()
+//        shape.path = path.cgPath
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
